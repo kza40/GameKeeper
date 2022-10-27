f51de8de4dcf6f29397f508005466161e2f07730
kza40 <101245568+kza40@users.noreply.github.com>
2022-10-27 12:42:06 -0700

    Initial commit

--- app/src/main/java/ca/cmpt276/myapplication/MainActivity.java
+++ app/src/main/java/ca/cmpt276/myapplication/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+//remove this line
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
