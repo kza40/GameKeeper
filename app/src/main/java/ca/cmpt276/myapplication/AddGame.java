c6ed777b6f068703167fd0339a3d20aec02a2444
Maria Zaman <mza114@sfu.ca>
2022-11-03 08:50:31 -0700

    Updated AddGame activity to support achievements preview.

--- app/src/main/java/ca/cmpt276/myapplication/AddGame.java
+++ app/src/main/java/ca/cmpt276/myapplication/AddGame.java
@@ -2,46 +2,78 @@ package ca.cmpt276.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+
 public class AddGame extends AppCompatActivity {
 
-    EditText edtP1Score;
-    EditText edtP2Score;
     EditText edtPScore;
     EditText edtGScore;
-    EditText edtGameName;
+    EditText edtConfigName;
+
+    Button btnPreview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_game);
 
+        btnPreview = findViewById(R.id.previewBtn);
 
         setupInputFields();
+        setupPreviewButton();
         setupSaveButton();
     }
 
     private void setupInputFields() {
-        edtP1Score = findViewById(R.id.p1Score);
-        edtP2Score = findViewById(R.id.p2Score);
         edtPScore = findViewById(R.id.poorScore);
         edtGScore = findViewById(R.id.goodScore);
-        edtGameName = findViewById(R.id.gameName);
+        edtConfigName = findViewById(R.id.configName);
 
+        edtPScore.addTextChangedListener(previewTextWatcher);
+        edtGScore.addTextChangedListener(previewTextWatcher);
+    }
+
+    private void setupPreviewButton() {
+        btnPreview.setOnClickListener(view -> {
+            Intent intent = PreviewAchievements.makeIntent(
+                        AddGame.this,
+                        Integer.parseInt(edtPScore.getText().toString()),
+                        Integer.parseInt(edtGScore.getText().toString())
+            );
+            startActivity(intent);
+        });
     }
 
     private void setupSaveButton() {
-        Button saveBtn = findViewById(R.id.saveBtn);
-        saveBtn.setOnClickListener(view -> {
+        Button btnSave = findViewById(R.id.saveBtn);
+        btnSave.setOnClickListener(view -> {
             //this is just for now
             Toast.makeText(AddGame.this, "Just a demo", Toast.LENGTH_LONG).show();
         });
     }
+
+    private TextWatcher previewTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            String pScoreInput = edtPScore.getText().toString();
+            String gScoreInput = edtGScore.getText().toString();
+
+            btnPreview.setEnabled(!pScoreInput.isEmpty() && !gScoreInput.isEmpty());
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {}
+    };
+
 }
 
