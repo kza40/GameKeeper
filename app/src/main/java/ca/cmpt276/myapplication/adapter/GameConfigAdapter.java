646f7c5cde7219e83c245afce46d61b462a685cb
Peeyush <peeyush_sharma@vancity.com>
2022-11-06 14:12:05 -0800

    Refactored code in GameConfigAdapter class

--- app/src/main/java/ca/cmpt276/myapplication/adapter/GameConfigAdapter.java
+++ app/src/main/java/ca/cmpt276/myapplication/adapter/GameConfigAdapter.java
@@ -51,40 +51,34 @@ public class GameConfigAdapter extends ArrayAdapter<GameConfig> {
     }
 
     private void setupClickListenersOnButton(ViewGroup parent, int position, ImageButton btnEdit, ImageButton btnDelete) {
-        btnEdit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        btnEdit.setOnClickListener((View view) ->{
                 Intent intent=AddConfig.makeIntent(context,true,position);
                 intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 context.startActivity(intent);
-            }
         });
 
-        btnDelete.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                AlertDialog.Builder builder = new AlertDialog.Builder(context);
-                builder.setMessage(R.string.confirm_dialog_message)
-                        .setTitle(R.string.confirm_dialog_title)
-                        .setPositiveButton(R.string.confirm, new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialogInterface, int i) {
-
-                                switch (i) {
-                                    case DialogInterface.BUTTON_POSITIVE:
-                                        ConfigManager.getInstance().removeConfigAtIndex(position);
-                                        notifyDataSetChanged();
-                                        ((ViewConfigs)context).setEmptyState();
-                                        break;
-                                }
-                            }
-                        }).
-                        setNegativeButton(android.R.string.no, null);
-
-                // Create the AlertDialog object and return it
-                AlertDialog dialog = builder.create();
-                dialog.show();
-            }
+        btnDelete.setOnClickListener((View view) -> {
+            showConfirmDeleteDialogBox(position);
         });
+    }
+
+    private void showConfirmDeleteDialogBox(int position) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setMessage(R.string.confirm_dialog_message)
+                .setTitle(R.string.confirm_dialog_title)
+                .setPositiveButton(R.string.confirm, (DialogInterface dialogInterface, int i) ->{
+
+                        switch (i) {
+                            case DialogInterface.BUTTON_POSITIVE:
+                                ConfigManager.getInstance().removeConfigAtIndex(position);
+                                notifyDataSetChanged();
+                                ((ViewConfigs)context).setEmptyState();
+                                break;
+                        }
+                }).
+                setNegativeButton(android.R.string.no, null);
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
