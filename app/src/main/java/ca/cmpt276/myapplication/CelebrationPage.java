62c61283417b926e919499562e1a6c1ffc609b56
kza40 <101245568+kza40@users.noreply.github.com>
2022-12-01 13:54:41 -0800

    [Kia] starting different themes for celebration page. No major breakthroughs as of this moment.

--- app/src/main/java/ca/cmpt276/myapplication/CelebrationPage.java
+++ app/src/main/java/ca/cmpt276/myapplication/CelebrationPage.java
@@ -11,38 +11,53 @@ import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import ca.cmpt276.myapplication.model.ConfigManager;
+
 public class CelebrationPage extends AppCompatActivity {
 
     private static final String ACHIEVEMENT_NAME = "CelebrationPage: Achievement name";
+    private String nextAchievement;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_celebration_page);
 
-        TextView tvName = findViewById(R.id.tvAchievementName);
-        String name = getIntent().getStringExtra(ACHIEVEMENT_NAME);
-        tvName.setText(name);
+        ConfigManager configManager = ConfigManager.getInstance();
+        String theme = configManager.getTheme();
 
-        startEffects();
-        setupReload();
+        if (theme.equals(ThemeSetting.THEME_FITNESS)) {
+            setContentView(R.layout.activity_celebration_page_fitness);
+        } else if (theme.equals(ThemeSetting.THEME_SPONGEBOB)) {
+            setContentView(R.layout.activity_celebration_page_spongebob);
+        } else {
+            setContentView(R.layout.activity_celebration_page_starwars);
+        }
+
+
+//        TextView tvName = findViewById(R.id.tvAchievementName);
+//        String name = getIntent().getStringExtra(ACHIEVEMENT_NAME);
+//        tvName.setText(name);
+//
+//        findNextAchievement();
+//        startEffects();
+//        setupReload();
     }
 
     private void startEffects() {
         MediaPlayer mp = MediaPlayer.create(CelebrationPage.this, R.raw.win_sound);
         mp.start();
 
-        ImageView rSaber;
-        ImageView gSaber;
+        ImageView leftAnim;
+        ImageView rightAnim;
 
-        rSaber = findViewById(R.id.rSaber);
-        gSaber = findViewById(R.id.gSaber);
+        leftAnim = findViewById(R.id.rSaber);
+        rightAnim = findViewById(R.id.gSaber);
 
         Animation rotateSlideR = AnimationUtils.loadAnimation(this, R.anim.slide_rotate_left);
         Animation rotateSlideL = AnimationUtils.loadAnimation(this, R.anim.slide_rotate_right);
 
-        rSaber.startAnimation(rotateSlideR);
-        gSaber.startAnimation(rotateSlideL);
+        leftAnim.startAnimation(rotateSlideR);
+        rightAnim.startAnimation(rotateSlideL);
     }
 
     private void setupReload() {
