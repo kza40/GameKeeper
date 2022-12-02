8bd069e8a3171fad29826bd2c0b6d06f6251e9e8
kza40 <101245568+kza40@users.noreply.github.com>
2022-12-01 16:21:22 -0800

    [Kia] different themes and animations added. UI marginalizing is left along with the ability to change the themes

--- app/src/main/java/ca/cmpt276/myapplication/CelebrationPage.java
+++ app/src/main/java/ca/cmpt276/myapplication/CelebrationPage.java
@@ -17,31 +17,46 @@ public class CelebrationPage extends AppCompatActivity {
 
     private static final String ACHIEVEMENT_NAME = "CelebrationPage: Achievement name";
     private String nextAchievement;
+    private String theme;
+    private ConfigManager configManager;
+    ImageView ivReload;
+    TextView tvName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        ConfigManager configManager = ConfigManager.getInstance();
-        String theme = configManager.getTheme();
+        configManager = ConfigManager.getInstance();
+        theme = configManager.getTheme();
 
         if (theme.equals(ThemeSetting.THEME_FITNESS)) {
             setContentView(R.layout.activity_celebration_page_fitness);
+            tvName = findViewById(R.id.tvAchievementName2);
+            ivReload = findViewById(R.id.ivReload2);
+
         } else if (theme.equals(ThemeSetting.THEME_SPONGEBOB)) {
             setContentView(R.layout.activity_celebration_page_spongebob);
+            tvName = findViewById(R.id.tvAchievementName1);
+            ivReload = findViewById(R.id.ivReload1);
+
         } else {
             setContentView(R.layout.activity_celebration_page_starwars);
+            tvName = findViewById(R.id.tvAchievementName);
+            ivReload = findViewById(R.id.ivReload);
         }
 
 
-//        TextView tvName = findViewById(R.id.tvAchievementName);
-//        String name = getIntent().getStringExtra(ACHIEVEMENT_NAME);
-//        tvName.setText(name);
-//
+        String name = getIntent().getStringExtra(ACHIEVEMENT_NAME);
+        tvName.setText(name);
+
 //        findNextAchievement();
-//        startEffects();
-//        setupReload();
+        startEffects();
+        setupReload();
     }
+
+//    private void findNextAchievement() {
+//
+//    }
 
     private void startEffects() {
         MediaPlayer mp = MediaPlayer.create(CelebrationPage.this, R.raw.win_sound);
@@ -49,9 +64,21 @@ public class CelebrationPage extends AppCompatActivity {
 
         ImageView leftAnim;
         ImageView rightAnim;
+        ImageView falconer;
+        if(theme=="THEME_FITNESS"){
+            leftAnim = findViewById(R.id.right_dumbbell);
+            rightAnim = findViewById(R.id.left_dumbbell);
+        } else if ( theme=="THEME_SPONGEBOB"){
+            leftAnim = findViewById(R.id.right_party);
+            rightAnim = findViewById(R.id.left_party);
+        } else {
+            leftAnim = findViewById(R.id.right_spaceship);
+            rightAnim = findViewById(R.id.left_spaceship);
+            falconer = findViewById(R.id.falconer);
+            Animation slideUp = AnimationUtils.loadAnimation(this, R.anim.up);
+            falconer.startAnimation(slideUp);
+        }
 
-        leftAnim = findViewById(R.id.rSaber);
-        rightAnim = findViewById(R.id.gSaber);
 
         Animation rotateSlideR = AnimationUtils.loadAnimation(this, R.anim.slide_rotate_left);
         Animation rotateSlideL = AnimationUtils.loadAnimation(this, R.anim.slide_rotate_right);
@@ -61,7 +88,7 @@ public class CelebrationPage extends AppCompatActivity {
     }
 
     private void setupReload() {
-        ImageView ivReload = findViewById(R.id.ivReload);
+        theme = configManager.getTheme();
         ivReload.setOnClickListener(view -> startEffects());
     }
 
